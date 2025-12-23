48f573c09ec21514ca11f4f4d6b27479bbf98c4d
ADISH <adishjain1107@gmail.com>
2025-12-24 00:34:42 +0530

    basic changes
    Signed-off-by: ADISH <adishjain1107@gmail.com>

--- models/books.go
+++ models/books.go
@@ -3,9 +3,10 @@ package models
 import (
 	"gorm.io/gorm"
 )
+
 // this book struct represents the books table in the database
 type Book struct {
-	ID        uint   `json:"id" gorm:"primaryKey"`
+	ID        uint   `json:"id" gorm:"primaryKey, autoIncrement"`
 	Author    string `json:"author"`
 	Title     string `json:"title" gorm:"not null"`
 	Publisher string `json:"publisher"`
@@ -15,4 +16,3 @@ type Book struct {
 func MigrateBooks(db *gorm.DB) error {
 	return db.AutoMigrate(&Book{})
 }
-
