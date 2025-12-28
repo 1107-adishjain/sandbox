0cb028560751b32922bec6219b7cefb4c65dba7a
ADISH <adishjain1107@gmail.com>
2025-12-28 18:01:54 +0530

    added created_At timestamp to improve the buisness logic and better debugging
    Signed-off-by: ADISH <adishjain1107@gmail.com>

--- models/books.go
+++ models/books.go
@@ -1,20 +1,20 @@
 package models
 
 import (
+	"time"
 	"gorm.io/gorm"
 )
 
 // this book struct represents the books table in the database
 type Books struct {
-	ID        uint   `json:"id" gorm:"primaryKey, autoIncrement"`
-	Author    string `json:"author" gorm:"not null" required:"true"`
-	Title     string `json:"title" gorm:"not null" reqquired:"true"`
-	Publisher string `json:"publisher" gorm:"not null" required:"true"`
-	Year      int    `json:"year" gorm:"not null" required:"true"`
+	ID        uint      `json:"id" gorm:"primaryKey, autoIncrement"`
+	Author    string    `json:"author" gorm:"not null" required:"true"`
+	Title     string    `json:"title" gorm:"not null" reqquired:"true"`
+	Publisher string    `json:"publisher" gorm:"not null" required:"true"`
+	Year      int       `json:"year" gorm:"not null" required:"true"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func MigrateBooks(db *gorm.DB) error {
 	return db.AutoMigrate(&Books{})
 }
-
-
